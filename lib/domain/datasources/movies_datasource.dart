7bf0faba36bed33a9cd708069d35fcf8f6c29dda
AlexanderJumbo <geossephyjumboalava@gmail.com>
2025-09-06 13:31:05 -0500

    first commit cinemapedia

---
+++ lib/domain/datasources/movies_datasource.dart
@@ -0,0 +1,11 @@
+
+
+//*No se podrá crear instancias de esta clase porque es abstracta
+//* Aquí se define los  orígenes de datos, movieDB, imdb, etc
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+abstract class MovieDatasource {
+
+  Future<List<Movie>> getNowPlaying({int page = 1});
+
+}
