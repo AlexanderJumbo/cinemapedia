0ab912404099a62f009efad83e8e77908af3c3e6
AlexanderJumbo <geossephyjumboalava@gmail.com>
2025-09-20 18:23:56 -0500

    Diseño de la pantalla - posterPath

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -3,24 +3,18 @@ import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.da
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 class MovieScreen extends ConsumerStatefulWidget {
-
   static const name = 'movie-screen';
 
   final String movieId;
 
-  const MovieScreen({
-    super.key, 
-    required this.movieId
-  });
+  const MovieScreen({super.key, required this.movieId});
 
   @override
   MovieScreenState createState() => MovieScreenState();
 }
 
 class MovieScreenState extends ConsumerState<MovieScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -30,18 +24,94 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     //* ref.watch(movieInfoProvider) -> Map<String, Movie>
     //* [widget.movieId] -> Movie
     final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
 
-    if(movie == null){
-      return Scaffold(body: const Center(child: CircularProgressIndicator(strokeWidth: 2,),));
+    if (movie == null) {
+      return Scaffold(
+          body: const Center(
+        child: CircularProgressIndicator(
+          strokeWidth: 2,
+        ),
+      ));
     }
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MovieID: ${widget.movieId}'),
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [
+          _CustomSliverAppbar(
+            movie: movie,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomSliverAppbar extends StatelessWidget {
+  final Movie movie;
+
+  const _CustomSliverAppbar({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7, //* el 70% de la pantalla
+      foregroundColor: Colors.white,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        title: Text(
+          movie.title,
+          style: const TextStyle(fontSize: 20, color: Colors.white),
+          textAlign: TextAlign.start,
+        ),
+        background: Stack(
+          children: [
+            SizedBox.expand(
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox.expand(
+              child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: AlignmentGeometry.topCenter,
+                          end: AlignmentGeometry.bottomCenter,
+                          stops: [
+                    0.7,
+                    1.0
+                  ], //* comienzan desde el 70% de la pantalla hasta abajo
+                          colors: [
+                    Colors.transparent,
+                    Colors.black87
+                  ]))),
+            ),
+            const SizedBox.expand(
+              child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: AlignmentGeometry.topLeft,
+                          stops: [
+                            0.0,
+                            0.3
+                          ], //* comienzan desde el 70% de la pantalla hasta abajo
+                          colors: [
+                            Colors.black87,
+                            Colors.transparent,
+                          ]
+                      )
+                    )
+                  ),
+            ),
+          ],
+        ),
       ),
     );
   }
