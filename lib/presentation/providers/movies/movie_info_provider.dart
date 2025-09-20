2a532b612877c6251c26238736e5dfbd6e78c863
AlexanderJumbo <geossephyjumboalava@gmail.com>
2025-09-20 17:50:09 -0500

    Petición HTTP y probando caché

--- lib/presentation/providers/movies/movie_info_provider.dart
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -7,23 +7,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final movieInfoProvider = StateNotifierProvider<MovieMapNotifier, Map<String,Movie>>((ref) {
   final movieRepository = ref.watch(movieRepisitoryProvider);
-
   return MovieMapNotifier(getMovie: movieRepository.getMovieById); //* otra forma de llamar a la referencia de la función
-
 });
-
 /*
-
   {
     'id1': Movie(),
     'id2': Movie(),
     'id3': Movie(),
     'id4': Movie()
   }
-
 */
-
-
 
 typedef GetMovieCallback = Future<Movie>Function(String movieId);
 
@@ -37,7 +30,7 @@ class MovieMapNotifier extends StateNotifier<Map<String, Movie>>{
 
   Future<void> loadMovie(String movieId) async{
     if(state[movieId] != null) return;
-
+    print('Realizando la petición HTTP');
     final movie = await getMovie(movieId);
 
     //* Creo un nuevo estado, y coloco el id de la película apuntando a {} de movie en memoria
